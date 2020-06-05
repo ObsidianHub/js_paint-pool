8b4df7ec5c8e3cf41f187ee0649e4685537a8bad
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-06-05 23:56:03 +0300

    create gradualShift method in Color

--- script.js
+++ script.js
@@ -35,4 +35,10 @@ class Color {
   getRGBString() {
     return String(this.r + "," + this.g + "," + this.b);
   }
+
+  gradualShift(direction) {
+    this.r = Math.floor(Math.abs(Math.cos(direction * 0.75) * 256));
+    this.g = Math.floor(Math.abs(Math.sin(direction * 0.25) * 256));
+    this.b = Math.floor(Math.abs(Math.sin(direction * 0.5) * 256));
+  }
 }
